02b816fe097e20270f81699f094c779ade6b2070
chenhonglin <chenhonglin@aliyun.com>
2021-12-28 23:57:06 +0800

    Modify build.rs since submodule added
    Signed-off-by: chenhonglin <chenhonglin@aliyun.com>

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-RTT_PATH=/Users/chenhonglin/dist/stm32h750-artpi-h750 cargo build --release --target thumbv7em-none-eabihf
+RTT_PATH=stm32h750-artpi-h750 cargo build --release --target thumbv7em-none-eabihf
