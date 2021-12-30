64e77530146282df76e71bcd4e7c70661063638b
chenhonglin <chenhonglin@xiaomi.com>
2021-12-30 15:21:43 +0800

    Fix bug in build.rs.
    Give a defualt RTT_PATH.
    
    Signed-off-by: chenhonglin <chenhonglin@xiaomi.com>

--- build.rs
+++ build.rs
@@ -6,7 +6,8 @@ use std::path::PathBuf;
 fn main() {
     println!("cargo:rerun-if-changed=wrapper.h");
 
-    let rtt_path = env::var("RTT_PATH").unwrap();
+    let rtt_path = env::var("RTT_PATH")
+		.unwrap_or(String::from("stm32h750-artpi-h750"));
 
     let bindings = bindgen::Builder::default()
         .header("wrapper.h")
