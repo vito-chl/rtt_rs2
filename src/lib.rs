7f9e2b48fca6efc7fe02c8e92d92197517185544
vitors <260437028@qq.com>
2022-05-09 14:01:25 +0800

    add test framework

--- src/lib.rs
+++ src/lib.rs
@@ -4,88 +4,85 @@
 //! that provides basic functions and FS, NET and DEVICE.
 //!
 //! You can use this library on embedded devices that support rt-thread
-//!
-//! You can specify a function for “ C ” to call
-//!
-//! # Example
-//! ```
-//! #![no_std]
-//!
-//! use rtt_rs2::*;
-//! entry!(main);
-//! fn main() { /*.....*/ }
-//! ```
 
-#![no_std]
+
+#![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(not(feature = "std"), cfg(not(test)))]
+
 #![feature(alloc_error_handler)]
 #![feature(allow_internal_unstable)]
 #![feature(const_fn_fn_ptr_basics)]
 #![feature(const_fn_trait_bound)]
 #![feature(linkage)]
-#![cfg(not(test))]
 #![allow(dead_code)]
 
-#[doc = "alloc by rt-thread"]
-#[global_allocator]
-static GLOBAL: malloc::RttAlloc = malloc::RttAlloc;
+mod puts;
 
 pub extern crate alloc;
-pub mod api;
-mod bind;
-pub mod malloc;
 
-pub mod mutex;
-pub mod out;
-pub mod param;
-pub mod queue;
-pub mod semaphore;
-pub mod thread;
+cfg_if::cfg_if! {
 
-mod prelude;
-pub use prelude::v1::*;
+    if #[cfg(not(feature = "std"))] {
+        #[doc = "Alloc by rt-thread"]
+        #[global_allocator]
+        static GLOBAL: malloc::RttAlloc = malloc::RttAlloc;
+        pub mod api;
+        mod bind;
 
-// TODO: review this enum
-#[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum RTTError {
-    ThreadStartupErr,
-    MutexTakeTimeout,
-    SemaphoreTakeTimeout,
-    QueueSendTimeout,
-    QueueReceiveTimeout,
-    OutOfMemory,
+        pub mod malloc;
+        pub mod mutex;
+        pub mod out;
+        pub mod param;
+        pub mod queue;
+        pub mod semaphore;
+        pub mod thread;
 
-    DeviceNotFound,
-    DeviceOpenFailed,
-    DeviceCloseFailed,
-    DeviceReadFailed,
-    DeviceWriteFailed,
-    DeviceTransFailed,
-    DeviceConfigFailed,
-    DeviceSetRxCallBackFailed,
-    DeviceSetTxCallBackFailed,
+        mod prelude;
+        pub use prelude::v1::*;
 
-    FuncUnDefine,
-}
+        // TODO: review this enum
+        #[derive(Debug, Copy, Clone, Eq, PartialEq)]
+        pub enum RTTError {
+            ThreadStartupErr,
+            MutexTakeTimeout,
+            SemaphoreTakeTimeout,
+            QueueSendTimeout,
+            QueueReceiveTimeout,
+            OutOfMemory,
 
-pub type RTResult<T> = Result<T, RTTError>;
+            DeviceNotFound,
+            DeviceOpenFailed,
+            DeviceCloseFailed,
+            DeviceReadFailed,
+            DeviceWriteFailed,
+            DeviceTransFailed,
+            DeviceConfigFailed,
+            DeviceSetRxCallBackFailed,
+            DeviceSetTxCallBackFailed,
 
-use crate::api::is_irq_context;
+            FuncUnDefine,
+        }
 
-fn panic_atomic_context(s: &str) {
-    if is_irq_context() {
-        panic!("{}", s);
+        pub type RTResult<T> = Result<T, RTTError>;
+
+        fn panic_atomic_context(s: &str) {
+            use crate::api::is_irq_context;
+            if is_irq_context() {
+                panic!("{}", s);
+            }
+        }
+
+        #[panic_handler]
+        #[inline(never)]
+        fn panic(info: &core::panic::PanicInfo) -> ! {
+            print!("{:}", info);
+            __rust_panic()
+        }
+
+        #[linkage = "weak"]
+        #[no_mangle]
+        fn __rust_panic() -> ! {
+            loop {}
+        }
     }
-}
-
-#[panic_handler]
-#[inline(never)]
-fn panic(info: &core::panic::PanicInfo) -> ! {
-    print!("{:}", info);
-    __rust_panic()
-}
-
-#[linkage = "weak"]
-#[no_mangle]
-fn __rust_panic() -> ! {
-    loop {}
 }
