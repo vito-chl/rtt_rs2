ca3487e6a684f6651fa94c6a303284d1828d18ac
vitors <260437028@qq.com>
2022-05-25 10:46:07 +0800

    add dfsdevops

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -1,6 +1,7 @@
 mod base;
 // mod device;
 mod cdevops;
+mod dfsdevops;
 mod interrupt;
 mod mem;
 mod mutex;
@@ -10,11 +11,12 @@ mod thread;
 
 pub use base::*;
 // pub use device::*;
+pub use cdevops::*;
+pub use cty::*;
+pub use dfsdevops::*;
 pub use interrupt::*;
 pub use mem::*;
 pub use mutex::*;
 pub use queue::*;
 pub use sem::*;
 pub use thread::*;
-
-pub use cty::*;
